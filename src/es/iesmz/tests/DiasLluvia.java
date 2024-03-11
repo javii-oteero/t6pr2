744e3eb9a74e591e90fb15208a3889c06aac9b26
javi <javii.oteero@gmail.com>
2024-03-11 11:51:35 +0100

    Initial commit con primer método

---
+++ src/es/iesmz/tests/DiasLluvia.java
@@ -0,0 +1,25 @@
+package es.iesmz.tests;
+
+public class DiasLluvia {
+
+    private boolean [][] haLLovido;
+
+    public DiasLluvia (){
+        this.haLLovido = new boolean[12][31];
+    }
+
+    public boolean registroDia (int dia, int mes, boolean lluvia){
+        if (mes >= 0 && mes <= 11){
+            if (mes == 1 && dia >= 0 && dia <= 27){
+                haLLovido[mes][dia] = lluvia;
+                return true;
+            } else if (mes % 2 == 0 && dia >= 0 && dia <= 30){
+                haLLovido[mes][dia] = lluvia;
+                return true;
+            } else if (dia >= 0 && dia <= 29){
+                haLLovido[mes][dia] = lluvia;
+                return true;
+            }
+        } return false;
+    }
+}
